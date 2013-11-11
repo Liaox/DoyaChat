e964bfe142d1b431eebbd4db55807ddc3f006ea8
daisy1754 <d-part@hotmail.co.jp>
2013-11-11 23:49:50 +0900

    コピペコードにhachikoプレフィックスが残ってたので消します

--- src/com/example/EmotionChat/DoyaPreferences.java
+++ src/com/example/EmotionChat/DoyaPreferences.java
@@ -26,25 +26,21 @@ public class DoyaPreferences {
         return PREFERENCES_NAME;
     }
 
-    public static boolean hasHachikoId(Context context) {
+    public static boolean hasId(Context context) {
         return getDefault(context).contains(DoyaPreferences.KEY_MY_ID);
     }
 
     /**
-     * @return 自身のHachikoId
-     * @throws IllegalStateException HachikoIDが存在しないとき
+     * @return 自身のId，未登録なら-1
      */
-    public static long getMyHachikoId(Context context) {
+    public static long getMyId(Context context) {
         long ret =  getDefault(context).getLong(DoyaPreferences.KEY_MY_ID, -1);
-        if (ret == -1) {
-            throw new IllegalStateException("Hachiko ID is not found on your device");
-        }
         return ret;
     }
 
     public static final String KEY_APP_VERSION = "app_version";
     public static final int APP_VERSION_DEFAULT = 0;
-    public static final String KEY_MY_ID = "my_hachiko_id";
+    public static final String KEY_MY_ID = "my_id";
 
     public static final String KEY_USE_FAKE_REQUEST_QUEUE = "use_fake_request_queue";
     public static final boolean USE_FAKE_REQUEST_QUEUE_DEFAULT = false;
